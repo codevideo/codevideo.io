f8167f975c00253f364af74e224dd126efb185c6
Chris Frewin <frewin.christopher@gmail.com>
2022-12-02 18:21:51 +0100

    fix capitalization

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import Layout from "../components/layout/Layout";
 import { Home } from "../components/pages/home/Home";
-import SEO from "../components/Seo";
+import SEO from "../components/SEO";
 
 export default function HomePage() {
   return (
