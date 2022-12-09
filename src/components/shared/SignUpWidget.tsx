cb6381a3e729a775da978e76e915e31099b2507f
Chris Frewin <frewin.christopher@gmail.com>
2022-12-09 11:43:58 +0100

    little toast on signup for now

--- src/components/shared/SignUpWidget.tsx
+++ src/components/shared/SignUpWidget.tsx
@@ -1,10 +1,20 @@
 import * as React from "react";
+import { toast } from "react-toastify";
 
 export interface ISignUpWidgetProps {}
 
 export function SignUpWidget(props: ISignUpWidgetProps) {
   const onClickGetEarlyAccess = () => {
-    window.open("https://forms.gle/6Q5Z7Z7Z7Z7Z7Z7Z7", "_blank");
+    toast(
+      <div>
+        🚀🚀🚀
+        <br />
+        Sign up coming soon!
+      </div>,
+      {
+        position: "top-center",
+      }
+    );
   };
 
   return (
@@ -13,7 +23,9 @@ export function SignUpWidget(props: ISignUpWidgetProps) {
         <div className="row justify-content-center">
           <div className="col-4">
             <p className="mt-5">
-              This is the <span className="text-primary">alpha release</span>. The full product will have many more features including custom styling, landscape mode, and more.
+              This is the <span className="text-primary">alpha release</span>.
+              The full product will have many more features including custom
+              styling, landscape mode, and more.
             </p>
             <button className="btn btn-primary" onClick={onClickGetEarlyAccess}>
               Get Early Access
